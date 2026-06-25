5369cae3096a28f73850369abe19fd26f84194a5
umair7109 <muhammadumair2354@gmail.com>
2026-06-25 16:11:17 +0500

    in progress

---
+++ app/src/config/Routeconfig.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from '../Pages/Login/Login'
+import Register from '../pages/Register/Register'
+import Products from '../pages/Products/Products'
+import Cart from '../pages/Cart/Cart'
+import ResponsiveAppBar from '../Coponents/Navbar'
+
+const Routeconfig = () => {
+  return (
+    <BrowserRouter>
+    <ResponsiveAppBar/>
+    <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='Register' element={<Register/>}/>
+        <Route path='Products' element={<Products/>}/>
+        <Route path='Cart' element={<Cart/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Routeconfig
